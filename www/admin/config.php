7b5ca6f8a92be035cab6e531a2b80fac1037fe7a
Anothereno <sevostyanick2011@yandex.ru>
2019-03-27 17:58:51 +0300

    site v1.0

---
+++ www/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://arthouzzz.ru/admin/');
+define('HTTP_CATALOG', 'http://arthouzzz.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://arthouzzz.ru/admin/');
+define('HTTPS_CATALOG', 'http://arthouzzz.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/arthouzzz.ru/www/admin/');
+define('DIR_SYSTEM', 'Z:/home/arthouzzz.ru/www/system/');
+define('DIR_LANGUAGE', 'Z:/home/arthouzzz.ru/www/admin/language/');
+define('DIR_TEMPLATE', 'Z:/home/arthouzzz.ru/www/admin/view/template/');
+define('DIR_CONFIG', 'Z:/home/arthouzzz.ru/www/system/config/');
+define('DIR_IMAGE', 'Z:/home/arthouzzz.ru/www/image/');
+define('DIR_CACHE', 'Z:/home/arthouzzz.ru/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/arthouzzz.ru/www/system/storage/download/');
+define('DIR_LOGS', 'Z:/home/arthouzzz.ru/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'Z:/home/arthouzzz.ru/www/system/storage/modification/');
+define('DIR_UPLOAD', 'Z:/home/arthouzzz.ru/www/system/storage/upload/');
+define('DIR_CATALOG', 'Z:/home/arthouzzz.ru/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'arthouzzz');
+define('DB_PASSWORD', '2846284Kk');
+define('DB_DATABASE', 'arthouzzz');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ www/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://arthouzzz.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://arthouzzz.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/arthouzzz.ru/www/catalog/');
+define('DIR_SYSTEM', 'Z:/home/arthouzzz.ru/www/system/');
+define('DIR_LANGUAGE', 'Z:/home/arthouzzz.ru/www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/arthouzzz.ru/www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/arthouzzz.ru/www/system/config/');
+define('DIR_IMAGE', 'Z:/home/arthouzzz.ru/www/image/');
+define('DIR_CACHE', 'Z:/home/arthouzzz.ru/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/arthouzzz.ru/www/system/storage/download/');
+define('DIR_LOGS', 'Z:/home/arthouzzz.ru/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'Z:/home/arthouzzz.ru/www/system/storage/modification/');
+define('DIR_UPLOAD', 'Z:/home/arthouzzz.ru/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'arthouzzz');
+define('DB_PASSWORD', '2846284Kk');
+define('DB_DATABASE', 'arthouzzz');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
